3c47d8a643eba1a5c9c6f83e6624ee47ca6a1c78
Talha Güller <talhaguller7@gmail.com>
2023-05-28 18:54:45 +0300

    odev

--- kur.py
+++ kur.py
@@ -4,7 +4,7 @@ conn = http.client.HTTPSConnection("api.collectapi.com")
 
 headers = {
     'content-type': "application/json",
-    'authorization': "apikey 71QXXfV2zSBV0HV0jAMPtQ:6gTmM6n0NkdZeRA8WvcaTC"
+    'authorization': "api-key"
     }
 
 conn.request("GET", "/economy/liveBorsa", headers=headers)
